7a3c678bea41d4442537f6675179480a20893b7c
eierapfel <eierapfel.125@gmail.com>
2019-05-10 04:24:55 +0900

    保存できなバグの修正

--- src/serialization/sb3.js
+++ src/serialization/sb3.js
@@ -54,7 +54,8 @@ const CORE_EXTENSIONS = [
     'operator',
     'procedures',
     'sensing',
-    'sound'
+	'sound',
+	'extension'
 ];
 
 // Constants referring to 'primitive' blocks that are usually shadows,
